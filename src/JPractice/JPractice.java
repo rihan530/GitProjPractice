6fb48ccba0768d1e6fa1b7a282b57313dc52f44b
Fensier <renovatio530@gmail.com>
2018-01-23 13:39:15 +0900

    - Scanner 로 입력받기 - 받은 입력값을 출력

--- src/JPractice/JPractice.java
+++ src/JPractice/JPractice.java
@@ -1,10 +1,21 @@
 package JPractice;
 
+import java.util.Scanner;
+
 public class JPractice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
 		System.out.println("안녕");
+		
+		// 입력
+		System.out.println("숫자를 입력하세요.");
+		int left = sc.nextInt();
+		System.out.println("숫자를 한번 더 입력하세요.");
+		int right = sc.nextInt();
+		// 출력
+		System.out.println("입력한 숫자: " + left + "," + right);
 		
 	}
 
